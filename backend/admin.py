b626f7ebde6b2d03d3b37604354196af063109fe
ShiXing Wang <wsxbj985@gmail.com>
2024-08-06 17:22:27 +0800

    detect photo api

--- backend/admin.py
+++ backend/admin.py
@@ -1,15 +1,4 @@
 from django.contrib import admin
 
 # Register your models here.
-from backend.models import UserInfo
 
-admin.site.site_header = '任务管理系统'
-
-
-class UserInfoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'username', 'password',)
-    list_display_links = ('username',)
-    list_per_page = 50
-
-
-admin.site.register(UserInfo, UserInfoAdmin)
