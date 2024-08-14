45b42ad1160236be88ce70d79f448e3305cfeeac
unknown <2592860500@qq.com>
2024-08-14 22:00:43 +0800

    latest

--- app_front/src/views/HomePage.vue
+++ app_front/src/views/HomePage.vue
@@ -2,28 +2,21 @@
     <div class="home">  
       <h1>保护动物，人人有责</h1>  
       <p>欢迎来到我们的保护动物网站，这里汇聚了关于如何保护野生动物的信息。</p>  
-      <button @click="showMoreInfo">了解更多</button> 
-      <component :is="tabComponent"></component> 
     </div>  
   </template>  
     
 <script>
-import AnimalsList from '../components/AnimalsList.vue';
-export default{
-    name: 'HomePage',  
+export default{ 
+    // 变量
     data(){
       return{
-        tabComponent:"",
       }
     },
-    methods: {  
-      showMoreInfo() {  
-        alert('请访问我们的博客和资源页面获取更多信息！'); 
-        this.tabComponent = this.tabComponent==""?"AnimalsList":"" 
-      }  
+    // 函数
+    methods: {   
     } ,
+    // 组件
     components:{
-      AnimalsList,
     }
   }  
 </script>  
