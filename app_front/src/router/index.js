f2efea8d88d55a0c16bf773f63a3364244e10cc9
unknown <2592860500@qq.com>
2024-09-03 10:40:04 +0800

    Merge homepage into frontend

--- app_front/src/router/index.js
+++ app_front/src/router/index.js
@@ -2,11 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../views/HomePage.vue'
 import AboutPage from '../views/AboutPage.vue'
 import DetectPage from '../views/DetectPage.vue'
-<<<<<<< HEAD
 // import AnimalsList from '../components/AnimalsList.vue' 
-=======
-//import AnimalsList from '../components/AnimalsList.vue' 
->>>>>>> homepage
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,20 +22,11 @@ const router = createRouter({
       name: 'Detect',  
       component: DetectPage  
     },    
-<<<<<<< HEAD
     // {  
     //   path: '/animals',  
     //   name: 'Animals',  
     //   component: AnimalsList  
     // },  
-=======
-    
-  //{  
-  //    path: '/animals',  
-     // name: 'Animals',  
-      //component: AnimalsList  
-    //},  
->>>>>>> homepage
   ]
 })
 
