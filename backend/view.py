10aca2189b8aee76a899a4c5dddebd0a377622c1
ShiXing Wang <wsxbj985@gmail.com>
2024-09-21 23:14:30 +0800

    little

--- backend/view.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from rest_framework.viewsets import ModelViewSet
-from backend.models import UserInfo
-from backend.serializer import UserInfoSerializer
-from backend.filter import UserInfoFilter
-from django_filters.rest_framework import DjangoFilterBackend
-
-
