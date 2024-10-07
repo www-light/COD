82c6f0094f87bf6ee183ed97995947f54c16db62
unknown <2592860500@qq.com>
2024-10-07 15:43:49 +0800

    latest change

--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from . import views
+from . import views1
 
 urlpatterns = [
     path('upload_images', views.upload_images.as_view()),
+    path('random_animal', views1.get_random_animal, name='random_aninal'),
 ]
